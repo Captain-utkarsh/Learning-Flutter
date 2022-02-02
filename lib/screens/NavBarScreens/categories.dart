8be6414141b197c9435379338dfc6396c45f5497
ZTLAB66 <ztlab66@ZTLAB66.local>
2022-02-02 18:59:40 +0530

    Switch, Checkbox,Drop down menu,Radio

--- lib/screens/NavBarScreens/categories.dart
+++ lib/screens/NavBarScreens/categories.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class Categories extends StatefulWidget {
-  const Categories({ Key? key }) : super(key: key);
+  const Categories({Key? key}) : super(key: key);
 
   @override
   _CategoriesState createState() => _CategoriesState();
@@ -11,44 +15,8 @@ class _CategoriesState extends State<Categories> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body :Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.05,
-            ),
-            const Text('Categories',style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.05,
-            ),
-            Image.asset('assets/images/categories.jpg'),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.05,
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.blueAccent,
-                elevation: 5.0,
-                shape: const BeveledRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(2))),
-              ),
-              onPressed: () {
-                //exit(0);
-                Navigator.pop(context);
-              },
-              child: const Text(
-                'Back ',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                    color: Colors.white),
-              ),
-            ),
-          ],
-        ),
+      body: const WebView(
+        initialUrl: 'https://flutter.dev',
       ),
     );
   }
--- lib/screens/NavBarScreens/more.dart
+++ lib/screens/NavBarScreens/more.dart
@@ -8,51 +8,204 @@ class MoreItems extends StatefulWidget {
 }
 
 class _MoreItemsState extends State<MoreItems> {
+  List<String> dropdownSublist = ['Kotlin', 'Java', 'Flutter'];
+  List<String> dropdownItems = ['Android', 'IOS', 'QA', 'Web', 'BA'];
+  String? dropdownValue = '';
+  bool? value1 = false;
+  bool? value2 = false;
+  bool? value3 = false;
+  bool? value4 = false;
+  int _radioval = 1;
+  bool _switchVal = false;
+  bool _switchVal2 = false;
+  bool _switchVal3 = false;
+
+  _MoreItemsState() {
+    dropdownValue = dropdownItems[0];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.05,
-            ),
-            const Text(
-              'More Items !',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
+        body: Center(
+      child: Column(
+        children: [
+          DropdownButton<String>(
+              isExpanded: false,
+              value: dropdownValue,
+              style: TextStyle(color: Colors.blueAccent),
+              elevation: 10,
+              borderRadius: BorderRadius.circular(5.0),
+              icon: const Icon(Icons.arrow_downward_rounded),
+              items:
+                  dropdownItems.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownValue = newValue;
+                });
+              }),
+          // Row(
+          //   children: [
+          //     Radio(value: 4, groupValue: 4, onChanged: (_){})
+          //   ],
+          // ),
+          CheckboxListTile(
+              controlAffinity: ListTileControlAffinity.leading,
+              title: Text(dropdownItems[0]),
+              value: value1,
+              onChanged: (bool? value) {
+                setState(() {
+                  value1 = value!;
+                });
+              }),
+          CheckboxListTile(
+              controlAffinity: ListTileControlAffinity.leading,
+              title: Text(dropdownItems[1]),
+              value: value2,
+              onChanged: (bool? value) {
+                setState(() {
+                  value2 = value!;
+                });
+              }),
+          CheckboxListTile(
+              controlAffinity: ListTileControlAffinity.leading,
+              title: Text(dropdownItems[2]),
+              value: value3,
+              onChanged: (bool? value) {
+                setState(() {
+                  value3 = value!;
+                });
+              }),
+          CheckboxListTile(
+              controlAffinity: ListTileControlAffinity.leading,
+              title: Text(dropdownItems[3]),
+              value: value4,
+              onChanged: (bool? value) {
+                setState(() {
+                  value4 = value!;
+                });
+              }),
+          Row(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 2.5,
+                height: MediaQuery.of(context).size.height * 0.05,
+                child: RadioListTile(
+                  value: 1,
+                  groupValue: _radioval,
+                  onChanged: (val) {
+                    setState(() {
+                      _radioval = val as int;
+                    });
+                  },
+                  title: Text(dropdownItems[0]),
+                ),
               ),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.05,
-            ),
-            Image.asset('assets/images/more.jpg'),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.025,
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.blueAccent,
-                elevation: 5.0,
-                shape: const BeveledRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(2))),
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 3,
+                height: MediaQuery.of(context).size.height * 0.05,
+                child: RadioListTile(
+                  value: 2,
+                  groupValue: _radioval,
+                  onChanged: (val) {
+                    setState(() {
+                      _radioval = val as int;
+                    });
+                  },
+                  title: Text(dropdownItems[1]),
+                ),
               ),
-              onPressed: () {
-                //exit(0);
-                Navigator.pop(context);
-              },
-              child: const Text(
-                'Back ',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                    color: Colors.white),
+
+              // RadioListTile(
+              //   value: 3,
+              //   groupValue: _radioval,
+              //   onChanged: (val) {
+              //     setState(() {
+              //       _radioval = val as int;
+              //     });
+              //   },
+              //   title: Text(dropdownItems[2]),
+              // ),
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 3,
+                height: MediaQuery.of(context).size.height * 0.05,
+                child: RadioListTile(
+                  value: 4,
+                  groupValue: _radioval,
+                  onChanged: (val) {
+                    setState(() {
+                      _radioval = val as int;
+                    });
+                  },
+                  title: Text(dropdownItems[3]),
+                ),
               ),
-            ),
-          ],
-        ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 3,
+                height: MediaQuery.of(context).size.height * 0.05,
+                child: RadioListTile(
+                  value: 5,
+                  groupValue: _radioval,
+                  onChanged: (val) {
+                    setState(() {
+                      _radioval = val as int;
+                    });
+                  },
+                  title: Text(dropdownItems[4]),
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Text(dropdownItems[0]),
+              const SizedBox(
+                width: 5.0,
+              ),
+              Switch(
+                  value: _switchVal,
+                  onChanged: (bool val) {
+                    setState(() {
+                      _switchVal = val;
+                    });
+                  }),
+              Text(dropdownItems[1]),
+              const SizedBox(
+                width: 5.0,
+              ),
+              Switch(
+                  value: _switchVal2,
+                  onChanged: (bool val) {
+                    setState(() {
+                      _switchVal2 = val;
+                    });
+                  }),
+              Text(dropdownItems[2]),
+              const SizedBox(
+                width: 5.0,
+              ),
+              Switch(
+                  value: _switchVal3,
+                  onChanged: (bool val) {
+                    setState(() {
+                      _switchVal3 = val;
+                    });
+                  }),
+            ],
+          )
+
+          ///RadioListTile(value: value, groupValue: groupValue, onChanged: onChanged)
+        ],
       ),
-    );
+    ));
   }
 }
